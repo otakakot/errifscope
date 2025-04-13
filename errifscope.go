1000af6491574ccda89b1e41f87efccd9d2c0f8e
otakakot <kotaro.otaka@gmail.com>
2025-04-14 07:52:59 +0900

    first commit

--- errifscope.go
+++ errifscope.go
@@ -30,94 +30,90 @@ func run(pass *analysis.Pass) (any, error) {
 	var previousStmt ast.Node
 
 	inspect.Preorder(nodeFilter, func(node ast.Node) {
-		switch n := node.(type) {
-		case *ast.IfStmt:
-			binaryExpr, ok := n.Cond.(*ast.BinaryExpr)
-			if !ok {
-				return
-			}
-
-			assignStmt, ok := previousStmt.(*ast.AssignStmt)
-			if !ok {
-				return
-			}
-
-			// for error variable check
-			errFlag := false
-
-			// for value variable check
-			valFlag := false
-
-			var errExpr ast.Expr
-
-			for _, lh := range assignStmt.Lhs {
-				typeof := pass.TypesInfo.TypeOf(lh)
-				if typeof == nil {
-					continue
-				}
-
-				if typeof.String() != "error" {
-					lhIdent, ok := lh.(*ast.Ident)
-					if !ok {
-						continue
-					}
-
-					if lhIdent.Name != "_" {
-						valFlag = true
-
-						continue
-					}
-				}
-
-				errFlag = true
-
-				errExpr = lh
-			}
-
-			if errFlag && valFlag {
-				return
-			}
-
-			typeof := pass.TypesInfo.TypeOf(errExpr)
-			if typeof == nil {
-				return
-			}
-
-			if typeof.String() != "error" {
-				return
-			}
-
-			errIdent, ok := errExpr.(*ast.Ident)
-			if !ok {
-				return
-			}
-
-			ifErrIdent, ok := binaryExpr.X.(*ast.Ident)
-			if !ok {
-				return
-			}
-
-			if errIdent.Name != ifErrIdent.Name {
-				return
-			}
-
-			if binaryExpr.Op.String() != "!=" {
-				return
-			}
-
-			yIdent, ok := binaryExpr.Y.(*ast.Ident)
-			if !ok {
-				return
-			}
-
-			if yIdent.Obj != nil {
-				return
-			}
-
-			pass.Reportf(node.Pos(), msg, errIdent.Name)
+		if ifStmt, ok := node.(*ast.IfStmt); ok {
+			processIfStatement(pass, ifStmt, previousStmt)
 		}
 		previousStmt = node
 	})
 
 	return nil, nil
+}
+
+func processIfStatement(
+	pass *analysis.Pass,
+	ifStmt *ast.IfStmt,
+	previousStmt ast.Node,
+) {
+	binaryExpr, ok := ifStmt.Cond.(*ast.BinaryExpr)
+	if !ok {
+		return
+	}
+
+	assignStmt, ok := previousStmt.(*ast.AssignStmt)
+	if !ok {
+		return
+	}
+
+	errExpr, isScopable := analyzeAssignment(pass, assignStmt)
+	if !isScopable {
+		return
+	}
+
+	if isErrorCheckValid(pass, binaryExpr, errExpr) {
+		errIdent := errExpr.(*ast.Ident)
+		pass.Reportf(ifStmt.Pos(), msg, errIdent.Name)
+	}
+}
+
+func analyzeAssignment(
+	pass *analysis.Pass,
+	assignStmt *ast.AssignStmt,
+) (ast.Expr, bool) {
+	var errExpr ast.Expr
+
+	hasErrorVar, hasValueVar := false, false
+
+	for _, lhs := range assignStmt.Lhs {
+		typeof := pass.TypesInfo.TypeOf(lhs)
+		if typeof == nil {
+			continue
+		}
+
+		if typeof.String() == "error" {
+			hasErrorVar = true
+			errExpr = lhs
+		} else if ident, ok := lhs.(*ast.Ident); ok && ident.Name != "_" {
+			hasValueVar = true
+		}
+	}
+
+	return errExpr, hasErrorVar && !hasValueVar
+}
+
+func isErrorCheckValid(
+	pass *analysis.Pass,
+	binaryExpr *ast.BinaryExpr,
+	errExpr ast.Expr,
+) bool {
+	if pass.TypesInfo.TypeOf(errExpr).String() != "error" {
+		return false
+	}
+
+	errIdent, ok := errExpr.(*ast.Ident)
+	if !ok {
+		return false
+	}
+
+	ifIdent, ok := binaryExpr.X.(*ast.Ident)
+	if !ok || errIdent.Name != ifIdent.Name {
+		return false
+	}
+
+	if binaryExpr.Op.String() != "!=" {
+		return false
+	}
+
+	yIdent, ok := binaryExpr.Y.(*ast.Ident)
+
+	return ok && yIdent.Obj == nil
 }
